f20b1b97a3a39d08de801eb05c1aa6cb43ab07d9
Sonette Yao <sonetteyao@gmail.com>
2022-05-17 12:17:56 +0800

    update styling; add initial DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+const numbers = document.querySelectorAll('.num');
+const operations = document.querySelectorAll('.opn');
+const clear = document.getElementById('ac');
+const delete = document.getElementById('c');
+const previous = document.getElementById('previous');
+const current = document.getElementById('current');
+
+function operate() {
+
+}
