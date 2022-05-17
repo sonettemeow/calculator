4cf4356b7ba3847557d24e51d7c14656ac81db21
Sonette Yao <sonetteyao@gmail.com>
2022-05-17 21:59:27 +0800

    change last row css to flexbox; add event listeners to buttons

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-const numbers = document.querySelectorAll('.num');
-const operations = document.querySelectorAll('.opn');
-const clear = document.getElementById('ac');
-const delete = document.getElementById('c');
-const previous = document.getElementById('previous');
-const current = document.getElementById('current');
+const numberBtns = document.querySelectorAll('.num');
+const operationBtns = document.querySelectorAll('.opn');
+const clearAll = document.getElementById('ac');
+const delNum = document.getElementById('c');
+const reverse = document.getElementById('op-int');
+let previousValue = document.getElementById('previous');
+let currentValue = document.getElementById('current');
 
-function operate() {
+//previousValue.innerHTML = 'EWAN KO NA';
+//currentValue.innerHTML = 'HAYSSS';
 
+currentValue = '';
+function append(number) {
+    currentValue = currentValue.toString() + number.toString();
 }
+
+function update() {
+    currentValue.innerHTML = currentValue;
+    console.log('Are ang current balyu: ' + currentValue);
+}
+
+numberBtns.forEach(button => button.addEventListener('click', () => {
+    append(button.innerText);
+    update();
+}))
+
+operationBtns.forEach(button => button.addEventListener('click', () => {
+    previousValue.innerHTML = button.innerText;
+}))
+
+clearAll.addEventListener('click', () => {
+    previousValue = '';
+    currentValue = '';
+})
+
+reverse.addEventListener('click', () => {
+    if (Math.sign(currentValue) === 1) {
+        currentValue * -1;
+    } else if (Math.sign(currentValue) === -1) {
+        currentValue * -1;
+    }
+})
